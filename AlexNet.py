d7489a0bf31ef8f2eff55dd4fde568e390f93dfc
leaves <leafmasi@vip.qq.com>
2018-10-19 01:06:40 +0800

    commit

--- AlexNet.py
+++ AlexNet.py
@@ -4,6 +4,7 @@
 # @File    : AlexNet.py
 # @Software: PyCharm
 
+import tensorflow as tf
 #此函数为输出当前层的参数（层名称，输出尺寸）
 def print_activations(t):
     print(t.op.name,' ',t.get_shape().as_list())
@@ -21,14 +22,84 @@ def inference(images):
     """
 
     parameters = []
-    #conv1
+    # conv1
     with tf.name_scope('conv1') as scope: #将scope内生成的Variable自动命名为
         # conv1/XXX，区分不同卷积层之间的组件
-        kernel = tf.Variable(tf.truncated_normal([11,11,3,96],dtype=tf.float32,stddev=1e-1),name='weight')
+        kernel = tf.Variable(tf.truncated_normal([11,11,3,96],dtype=tf.float32,stddev=1e-1),name='weights')
         #conv1的核是11*11，3是输入channel数（后面的函数run_benchmark()中指定了image的channel是3）,
         # 96是输出channel数.stddev标准差0.1
         conv = tf.nn.con2d(images,kernel,[1,4,4,1],padding='SAME')
-        biases = tf.Variable(tf.constant(0.0,shape=[96],dtype=tf.float32),trainable=Ture,name='biase')
+        biases = tf.Variable(tf.constant(0.0,shape=[96],dtype=tf.float32),trainable=Ture,name='biases')
         #初始化biases，一维向量，96个元素，全为0
         bias = tf.nn.bias_add(conv,biase) #把初始biases加到conv
-        conv1 = tf
+        conv1 = tf.nn.relu(bias,name=scope)
+        print_activations(conv1) #输出当前层参数
+        parameter += [kernel,biases] #更新权重和偏差
+
+    # 考虑到LRN层效果不明显，而且会让forward和backwood的速度大大下降，所以没有用LRN层
+    # pool1
+    pool1 = tf.nn.max_pool(conv1,
+                          ksize=[1,3,3,1],
+                          strides=[1,2,2,1],
+                          padding='VALID',
+                          name='pool1')
+    print_activations(pool1)
+
+    # conv2
+    with tf.name_scope('conv2') as scope:
+        kernel = tf.Variable(tf.truncated_normal([5,5,96,256],dtype=tf.float32,stddev=1e-1),name='weights')
+        conv = tf.nn.conv2d(pool1, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv2 = tf.nn.relu(bias, name=scope)
+        parameters += [kernel, biases]
+    print_activations(conv2)
+
+    # pool2
+    pool2 = tf.nn.max_pool(conv2,
+                           ksize=[1, 3, 3, 1],
+                           strides=[1, 2, 2, 1],
+                           padding='VALID',
+                           name='pool2')
+    print_activations(pool2)
+
+    # conv3
+    with tf.name_scope('conv3') as scope:
+        kernel = tf.Variable(tf.truncated_normal([3, 3, 256, 384],
+                                                 dtype=tf.float32,
+                                                 stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(pool2, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[384], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv3 = tf.nn.relu(bias, name=scope)
+        parameters += [kernel, biases]
+    print_activations(conv3)
+
+    # conv4
+    with tf.name_scope('conv4') as scope:
+        kernel = tf.Variable(tf.truncated_normal([3, 3, 384, 384], dtype=tf.float32, stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(conv3, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[384], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv4 = tf.nn.relu(bias, name=scope)
+        parameters += [kernel, biases]
+    print_activations(conv4)
+
+    # conv5
+    with tf.name_scope('conv5') as scope:
+        kernel = tf.Variable(tf.truncated_normal([3, 3, 384, 256], dtype=tf.float32, stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(conv4, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv5 = tf.nn.relu(bias, name=scope)
+        parameters += [kernel, biases]
+    print_activations(conv5)
+
+    # pool5
+    pool5 = tf.nn.max_pool(conv5,
+                           ksize=[1, 3, 3, 1],
+                           strides=[1, 2, 2, 1],
+                           padding='VALID',
+                           name='pool5')
+    print_activations(pool5)
+    return pool5, parameters
---
+++ confusion.md
@@ -0,0 +1,6 @@
+- PyCharm中为什么每次new一个project时都要创建一个scratch.py文件
+- v = tf.variable(3,name='v') 中定义name有什么用
+- batch训练的含义，or分成batch训练有什么好处
+- AlexNet代码中conv层中kernel也是variable，那kernel在后面训练中也是可变的？
+- AlexNet 的code第一层最后更新parameter没怎么懂，作用何在？
+- 前面对于一些tf参数例如tf.Variable参数的定义仅仅是在建模，即搭建流图吗？
